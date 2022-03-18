7ec7d80cefaedd73ef9a120571524df5651b8cfc
Kristian Lundkvist <kristian.lundkvist@gmail.com>
2022-03-18 14:20:17 +0100

    Rudimentory Details page.

--- FrontendRazorPages/Pages/Details.cshtml.cs
+++ FrontendRazorPages/Pages/Details.cshtml.cs
@@ -8,16 +8,50 @@ namespace FrontendRazorPages.Pages
     public class DetailsModel : PageModel
     {
 
+        public IHttpClientFactory _httpClientFactory { get; set; }
         private readonly IMemoryCache _cache;
+        public VehicleDO? Vehicle { get; set; }
 
-        public DetailsModel(IMemoryCache cache)
+        public DetailsModel(IHttpClientFactory httpClientFactory, IMemoryCache cache)
         {
+            _httpClientFactory = httpClientFactory;
             _cache = cache;
         }
         public async Task<IActionResult> OnGet(string VIN)
         {
+            if (_cache.TryGetValue("Vehicle DO " + VIN, out VehicleDO vehicle))
+            {
+                Vehicle = vehicle;
+                return Page();
+            }
+            if (_cache.TryGetValue("List Vehicle DO", out List<VehicleDO> vehicles))
+            {
+                Vehicle = vehicles.Find(v => v.VIN == VIN);
+                if (Vehicle != null)
+                {
+                    return Page();
+                }
+            }
 
-            _cache.Remove("List Vehicle DO");
+            var httpRequestMessage = new HttpRequestMessage(HttpMethod.Get, "http://localhost:5187/api/Vehicle/" + VIN);
+            var httpClient = _httpClientFactory.CreateClient();
+            var httpResponseMessage = await httpClient.SendAsync(httpRequestMessage);
+
+            if (httpResponseMessage.IsSuccessStatusCode)
+            {
+                if (httpResponseMessage.Content != null)
+                {
+                    Vehicle = httpResponseMessage.Content.ReadFromJsonAsync<VehicleDO>().Result;
+
+                    var cacheOptions = new MemoryCacheEntryOptions().SetSlidingExpiration(TimeSpan.FromSeconds(2)).SetAbsoluteExpiration(TimeSpan.FromSeconds(5));
+                    _cache.Set("Vehicle DO " + VIN, Vehicle, cacheOptions);
+                }
+            }
+            else
+            {
+                return NotFound();
+            }
+
             return Page();
         }
     }
